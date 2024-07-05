d59a81b795d9e47c71975c9506f3cc697dcf1c18
tompelletreauduris <tpelletreau@ensc.fr>
2024-07-05 15:30:46 +0200

    test

--- test_snellius.sh
+++ test_snellius.sh
@@ -4,26 +4,21 @@
 #SBATCH --gpus=1
 #SBATCH --job-name=TestJob
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
 #SBATCH --time=01:00:00
 #SBATCH --output=slurm_output_%A.out
 
-
 module purge
 module load 2023
-module load  Anaconda3/2023.07-2
+module load Anaconda3/2023.07-2
 
+# This loads the anaconda virtual environment with our packages
+# source $HOME/.bashrc
+# conda activate
+
+source activate GNN_gpu
 
 #print the GPU driver version
 nvidia-smi --query-gpu=driver_version --format=csv,noheader
-
-
-## For Lisa and Snellius, modules are usually not needed
-## https://userinfo.surfsara.nl/systems/shared/modules 
-
-# This loads the anaconda virtual environment with our packages
-source $HOME/.bashrc
-conda activate
 
 #print the python version
 python --version
