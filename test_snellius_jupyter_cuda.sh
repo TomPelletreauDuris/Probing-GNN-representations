c1b996ded92276ac75eb5b789db07d2ae466a762
tompelletreauduris <tpelletreau@ensc.fr>
2024-07-23 17:27:29 +0200

    gpu

--- test_snellius_jupyter_cuda.sh
+++ test_snellius_jupyter_cuda.sh
@@ -10,15 +10,15 @@
 module purge
 module load 2022
 module load CUDA/11.7.0
-module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 # module load Anaconda3/2022.05
 # module load Anaconda3/2023.07-2
 #module load IPython/8.14.0-GCCcore-12.3.0
 # module load JupyterHub/4.0.2-GCCcore-12.3.0
 module load IPython/8.5.0-GCCcore-11.3.0
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 module load JupyterHub/3.0.0-GCCcore-11.3.0
 
-source activate GNN_gpu
+# source activate GNN_gpu
 
 # Choose random port and print instructions to connect
 PORT=`shuf -i 5000-5999 -n 1`
