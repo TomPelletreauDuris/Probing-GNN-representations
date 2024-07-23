34d39f446032dd28257f872bc784097f355a5658
tompelletreauduris <tpelletreau@ensc.fr>
2024-07-23 17:06:11 +0200

    gpu

--- test_snellius_jupyter_cuda.sh
+++ test_snellius_jupyter_cuda.sh
@@ -14,7 +14,9 @@ module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 # module load Anaconda3/2022.05
 # module load Anaconda3/2023.07-2
 #module load IPython/8.14.0-GCCcore-12.3.0
-module load JupyterHub/4.0.2-GCCcore-12.3.0
+# module load JupyterHub/4.0.2-GCCcore-12.3.0
+module load IPython/8.5.0-GCCcore-11.3.0
+module load JupyterHub/3.0.0-GCCcore-11.3.0
 
 source activate GNN_gpu
 
