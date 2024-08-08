befc9eef80776f279ac613e75536dfb2b1034fce
tompelletreauduris <tpelletreau@ensc.fr>
2024-08-08 12:28:33 +0200

    changes

--- test_gin.py
+++ test_gin.py
@@ -1,8 +1,64 @@
 
+from Datasets.FC.create_dataset import read_dataset
+
+dataset = read_dataset()
+
+len(dataset)
+
+import torch
+torch.manual_seed(37)
+
 DATASET = "FC"
 
+MODEL = "GIN"
+from models.models_FC import GIN_framework as framework # import the model
+gnn = framework(dataset)
+
+MODELbis = "GINbis"
+from models.models_FC import GIN_framework_bis as framework # import the model
+gnnbis = framework(dataset)
+
+MODELtri = "GINtri"
+from models.models_FC import GIN_framework_tri as framework # import the model
+gnntri = framework(dataset)
+
+MODEL2 = "GIN2"
+from models.models_FC import GIN_framework2 as framework2 # import the model
+gnn2 = framework2(dataset)
+
 MODEL3 = "GIN3"
+from models.models_FC import GIN_framework3 as framework3 # import the model
+gnn3 = framework3(dataset)
+
+gnn.load_model(path="models/"+DATASET+"_"+MODEL+"server.pt")
+
+gnn2.load_model(path="models/"+DATASET+"_"+MODEL2+"server.pt")
+
+gnn3.load_model(path="models/"+DATASET+"_"+MODEL3+"server.pt")
+
+gnnbis.load_model(path="models/"+DATASET+"_"+MODELbis+"server.pt")
+
+gnntri.load_model(path="models/"+DATASET+"_"+MODELtri+"server.pt")
+
+
+gnn.evaluate()
+
+
+gnnbis.evaluate()
+
+
+gnntri.evaluate()
+
+gnn2.evaluate()
+
+gnn3.evaluate()
+
 MODEL = MODEL3
+
+train_features, test_features = gnn3.evaluate_with_features2()
+
+print(len(train_features[0]))
+len(train_features), len(test_features)
 
 import pickle as pkl
 import networkx as nx
@@ -17,3 +73,53 @@ with open("results/"+DATASET+"_"+MODEL+"_test_properties.pkl", "rb") as f:
 print(len(train_properties))
 print(train_properties[0:5])
 
+# %%
+
+def calculate_avg_path_length(G):
+    if nx.is_connected(G):
+        return nx.average_shortest_path_length(G)
+    else:
+        # Alternative metrics for disconnected graphs
+        # Option 1: Use the average path length of the largest connected component
+        components = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+        largest_component = max(components, key=len)
+        return nx.average_shortest_path_length(largest_component)
+    
+
+def compute_graph_properties(data):
+    properties = []
+    for graph_data in data:
+        G = nx.from_edgelist(graph_data.edge_index.t().tolist())
+        num_nodes = G.number_of_nodes()
+        num_edges = G.number_of_edges()
+        density = nx.density(G)
+        avg_path_len = calculate_avg_path_length(G)
+        num_cliques = len(list(nx.find_cliques(G)))
+        num_triangles = sum(nx.triangles(G).values()) / 3
+        num_squares = sum(nx.square_clustering(G).values()) / 4
+        number_of_node_in_the_largest_fully_connected_component = len(max(nx.connected_components(G), key=len))
+        if nx.is_connected(G):
+            small_world = nx.algorithms.smallworld.sigma(G)
+        else:
+            components = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+            largest_component = max(components, key=len)
+            small_world = nx.algorithms.smallworld.sigma(largest_component)
+
+        properties.append((num_nodes, num_edges, density, avg_path_len, num_cliques, num_triangles, num_squares, number_of_node_in_the_largest_fully_connected_component, small_world))
+    return properties
+
+train_idx_list = gnn.train_idx.tolist()
+selected_dataset = [gnn.dataset[i] for i in train_idx_list]
+train_properties = compute_graph_properties(selected_dataset)
+test_idx_list = gnn.test_idx.tolist()
+selected_dataset = [gnn.dataset[i] for i in test_idx_list]
+test_properties = compute_graph_properties(selected_dataset)
+
+with open("results/"+DATASET+"_"+MODEL+"_train_properties_with_sm.pkl", "wb") as f:
+    pkl.dump(train_properties, f)
+
+with open("results/"+DATASET+"_"+MODEL+"_test_properties_with_sm.pkl", "wb") as f:
+    pkl.dump(test_properties, f)
+
+print(train_properties[0:5])
+print('finished')
