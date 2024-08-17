aa33052cf6058b94febd36c19498c0baf3ebe2ed
tompelletreauduris <tpelletreau@ensc.fr>
2024-08-17 19:30:35 +0200

    mm

--- test_snellius_GCN.sh
+++ test_snellius_GCN.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -t 30:00:00
+#SBATCH -t 10:00:00
 #SBATCH -N 1
 #SBATCH -p gpu_a100
 #SBATCH --gpus-per-node=1
@@ -9,18 +9,29 @@
 
 module purge
 module load 2022
+# module load Anaconda3/2023.07-2
+# module load CUDA/12.1.1
+# module load cuDNN/8.9.2.26-CUDA-12.1.1
 module load CUDA/11.7.0
 module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+
+# This loads the anaconda virtual environment with our packages
+# source $HOME/.bashrc
+# conda activate
 
 source activate GNN_gpu
 
 srun python -uc "import torch; print('GPU available?', torch.cuda.is_available())"
 
-# Print the GPU driver version
+#print the GPU driver version
 nvidia-smi --query-gpu=driver_version --format=csv,noheader
 
-# Print the Python version
+#print the python version
 python --version
 
-# Run the actual experiment
-python FC_probing_GCN.py
+# Run the actual experiment. 
+# python FC_probing_GCN.py
+# python <<EOF
+# import torch
+# print(torch.cuda.is_available())
+# EOF
