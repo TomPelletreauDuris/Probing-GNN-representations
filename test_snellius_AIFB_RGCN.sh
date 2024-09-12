87d7421bc9d12a6c506009eac60143af08af3a0e
tompelletreauduris <tpelletreau@ensc.fr>
2024-09-12 17:47:25 +0200

    AIFB RGCN

---
+++ test_snellius_AIFB_RGCN.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -t 20:00:00
+#SBATCH -N 1
+#SBATCH -p genoa
+#SBATCH -o jupyter-notebook-py-CLINTOX_GCN.out
+
+module purge
+# module load OpenSSL/1.1
+module load 2022
+module load CUDA/11.7.0
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+
+# source activate GNN_gpu
+
+srun python -uc "import torch; print('GPU available?', torch.cuda.is_available())"
+
+# Print the GPU driver version
+nvidia-smi --query-gpu=driver_version --format=csv,noheader
+
+# Print the Python version
+python --version
+
+# Run the actual experiment
+python AIFB_probing_RGCN.py
