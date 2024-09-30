ce23ce3ba5a2841fdffe2d1cf643ee3983dd83a2
tompelletreauduris <tpelletreau@ensc.fr>
2024-09-30 18:29:04 +0200

    files for sbatch jobs

---
+++ snellius_gcn_dropout.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH -p genoa
+## SBATCH --gpus-per-node=1
+#SBATCH -o gcndropout-cuda.out
+
+
+module purge
+module load 2022
+module load CUDA/11.7.0
+# module load Anaconda3/2022.05
+# module load Anaconda3/2023.07-2
+#module load IPython/8.14.0-GCCcore-12.3.0
+# module load JupyterHub/4.0.2-GCCcore-12.3.0
+module load IPython/8.5.0-GCCcore-11.3.0
+module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+# module load JupyterHub/3.0.0-GCCcore-11.3.0
+
+# source activate GNN_gpu
+
+python3 Probing_GCN_dropout.py
